1c9de449bfebad1491165dcec67a46b3861c7f0b
Barbarian <yogeshsmodak@gmail.com>
2017-02-07 18:20:18 +0530

    committed

--- blog/models.py
+++ blog/models.py
@@ -3,15 +3,30 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    author=models.ForeignKey('auth.User')
-    title=models.CharField(max_length=100)
-    text=models.TextField()
-    cdate=models.DateTimeField(default=timezone.now)
-    pdate=models.DateTimeField(blank=True,null=True)
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    cdate = models.DateTimeField(default=timezone.now)
+    pdate = models.DateTimeField(blank=True,null=True)
 
     def publish(self):
-        self.pdate=timezone.now()
+        self.pdate = timezone.now()
         self.save()
 
         def __str__(self):
             return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    cdate = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
